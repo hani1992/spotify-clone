7245b19e3a3c9b784c6ef0dcd45ce178c3c61eca
hani hinnawi <hani.hinnawi@outlook.com>
2020-10-28 14:32:30 +0200

    new develop

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer, { initialState } from './reducer';
+import { DataLayer } from './DataLayer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState} reducer= {reducer} >
+      <App />
+    </DataLayer>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
